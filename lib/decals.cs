d68acfb490de890d403a14de7627a851928e05cc
zapk <zapkraft@gmail.com>
2017-11-29 21:10:56 -0800

    Initial upload

---
+++ lib/decals.cs
@@ -0,0 +1,153 @@
+if(isFile("Add-Ons/System_ReturnToBlockland/server.cs")) {
+	if(!$RTB::RTBR_ServerControl_Hook) {
+		exec("Add-Ons/System_ReturnToBlockland/RTBR_ServerControl_Hook.cs");
+	}
+	
+	RTB_registerPref("Decal Limit", "Decal System", "$DecalSystem::DecalLimit", "int 0 2000", "Support_Decals", 200, 0, 1);
+	RTB_registerPref("Decal Timeout (MS)", "Decal System", "$DecalSystem::DecalTimeout", "int -1 300000", "Support_Decals", 300000, 0, 1);
+} 
+else {
+	$DecalSystem::DecalLimit = 200;
+	$DecalSystem::DecalTimeout = 300000;
+} 
+
+function spawnDecal(%dataBlock, %position, %vector, %size, %effect) {
+	if (!isObject(MissionCleanup)) {
+		error("ERROR: MissionCleanup not created.");
+		return -1;
+	}
+
+	if (!isObject(%dataBlock) || %dataBlock.getClassName() !$= StaticShapeData) {
+		return;
+	}
+
+	if (%size $= "") {
+		%size = %dataBlock.decalSize;
+	}
+
+	if (%size $= "") {
+		%size = 1;
+	}
+
+	%obj = new StaticShape() {
+		dataBlock = %dataBlock;
+		spawnTime = $Sim::Time;
+	};
+
+	if (!isObject(%obj)) {
+		error("ERROR: Unable to create object.");
+		return -1;
+	}
+
+	if (!isObject(DecalGroupNew)) {
+		MissionCleanup.add(new SimGroup(DecalGroupNew));
+	}
+
+	DecalGroupNew.add(%obj);
+
+	while (DecalGroupNew.getCount() >= $DecalSystem::DecalLimit) {
+		//DecalGroupNew.getObject(0).delete();
+
+		//Port [09-10-13 10:27]: trying to fix decal limit,
+		//let's reverse this - since it seems to delete the newest decals
+
+		//DecalGroupNew.getObject(DecalGroupNew.getCount() - 1).delete();
+		%count = DecalGroupNew.getCount();
+
+		for (%i = 0; %i < %count; %i++) {
+			%decal = DecalGroupNew.getObject(%i);
+
+			if (%decal.spawnTime < %best || %best $= "") {
+				%best = %decal.spawnTime;
+				%oldest = %decal;
+			}
+		}
+
+		if (isObject(%oldest)) {
+			%oldest.delete();
+		}
+	}
+
+	if (!isObject(%obj)) {
+		return -1;
+	}
+
+	%obj.setTransform(%position SPC vectorToAxis(%vector));
+	%obj.setScale(%size SPC %size SPC %size);
+	%obj.normal = %vector;
+	if (%dataBlock.doColorShift) {
+		%obj.setNodeColor("ALL", %dataBlock.colorShiftColor);
+	}
+
+	if ($DecalSystem::DecalTimeout >= 0 && !%effect) {
+		%obj.schedule($DecalSystem::DecalTimeout - 1000, fadeOut);
+		%obj.schedule($DecalSystem::DecalTimeout, delete);
+	}
+
+	return %obj;
+}
+
+function spawnDecalFromRayCast(%dataBlock, %rayCast, %size) {
+	if (%rayCast $= 0) {
+		return -1;
+	}
+
+	return spawnDecal(%dataBlock, getWords(%rayCast, 1, 3), getWords(%rayCast, 4, 6), %size);
+}
+
+function sprayDecalWithRayCast(%dataBlock, %a, %b, %mask, %avoid, %size) {
+	return spawnDecalFromRayCast(%dataBlock, containerRayCast(%a, %b, %mask, %avoid), %size);
+}
+
+function vectorToAxis(%vector) {
+	%y = mRadToDeg(mACos(getWord(%vector, 2) / vectorLen(%vector))) % 360;
+	%z = mRadToDeg(mATan(getWord(%vector, 1), getWord(%vector, 0)));
+
+	%euler = vectorScale(0 SPC %y SPC %z, $pi / 180);
+	return getWords(matrixCreateFromEuler(%euler), 3, 6);
+}
+
+function clearDecals()
+{
+	if (isObject(DecalGroup)) 
+		DecalGroup.deleteAll();
+
+	if (isObject(DecalGroupNew)) 
+		DecalGroupNew.deleteAll();
+}
+
+function fadeAnimationLoop(%this, %time, %scale) {
+	cancel(%this.fadeAnimationLoop);
+	if (!isObject(%this)) {
+		return;
+	}
+	if (%scale $= "") {
+		%scale = %this.getScale();
+	}
+	if (%scale <= 0.1) {
+		%this.delete();
+		return;
+	}
+	if (!%time || %time == 0) {
+		%time = 16;
+	}
+
+	%scale = vectorSub(%scale, "0.15 0.15 0.15");
+	%this.setScale(%scale);
+	%this.fadeAnimationLoop = schedule(%time, 0, fadeAnimationLoop, %this, %time, %this.getScale());
+}
+
+function serverCmdClearDecals(%this) {
+	if (!%this.isAdmin) {
+		messageClient(%this, '', "\c3You are not allowed to use this command.");
+		return;
+	}
+	if (!isObject(DecalGroup)) {
+		%decals = DecalGroupNew.getCount();
+	}
+	else {
+		%decals = DecalGroup.getCount() + DecalGroupNew.getCount();
+	}
+	messageAll('MsgClearBricks', "\c3" @ %this.getPlayerName() @ "\c2 cleared all decals on the server." SPC "(" @ %decals @ " decals)");
+	clearDecals();
+}
---
+++ server.cs
@@ -0,0 +1,370 @@
+//   _____ _        _     _
+//  / ____| |      (_)   | | 
+// | (___ | |_ _ __ _  __| | ___
+//  \___ \| __| '__| |/ _` |/ _ \
+//  ____) | |_| |  | | (_| |  __/
+// |_____/ \__|_|  |_|\__,_|\___|
+//
+//
+// Author: Zapk
+// Version: 1.0.0 (September 2015)
+
+// An improved version of Port's old footstep script.
+// -  Customizable per paint color, with optional footprints.
+
+// Events:
+// - setMaterial [material] [hasFootprints]
+
+// Commands:
+// - /clearfootprints - clears all Stride material settings (Admin)
+// - /setfootstep [material] [hasFootprints] - changes the material setting of your current paint colour (Admin)
+
+exec("./lib/color.cs"); // RGB -> HEX conversion methods.
+exec("./lib/decals.cs"); // Port's decal system. Used for footprints.
+
+if(isFile("Add-Ons/System_ReturnToBlockland/server.cs") && !$RTB::Hooks::ServerControl)
+{
+	exec("Add-Ons/System_ReturnToBlockland/hooks/serverControl.cs");
+}
+
+// No, this won't cause an error if RTB doesn't exist.
+//    allGameScripts.cs.dso :
+//    "function RTB_registerPref(%displayName, %category, %varName, %varType, %varCategory, %defaultValue, %a, %b)"
+RTB_registerPref("Default Material", "Stride", "$Pref::Stride::DefaultMaterial", "string 32", "Server_Stride", "concrete", 0, 0);
+RTB_registerPref("Default Footprints", "Stride", "$Pref::Stride::DefaultFootprints", "bool", "Server_Stride", false, 0, 0);
+
+datablock StaticShapeData(FootprintDecal)
+{
+	shapeFile = "Add-Ons/Server_Stride/res/shapes/footprint.dts";
+	doColorShift = false;
+
+	decalSize = 1.0;
+};
+
+if(isPackage( "footstepPackage" ))
+{
+	deactivatePackage( "footstepPackage" );
+}
+
+package footstepPackage
+{
+	function gameConnection::spawnPlayer( %this )
+	{
+		parent::spawnPlayer( %this );
+		%this.player.fs_velocityCheckTick();
+	}
+
+	function Armor::onTrigger( %this, %obj, %slot, %val )
+	{
+		parent::onTrigger( %this, %obj, %slot, %val );
+
+		if ( %slot == 4 && !%this.canJet )
+		{
+			%val = false;
+		}
+
+		%obj.fs_trigger[ %slot ] = %val;
+	}
+};
+
+activatePackage( "footstepPackage" );
+
+function Player::getRightVector( %this )
+{
+	%vec = %this.getForwardVector();
+
+	%x = getWord(%vec, 0);
+	%y = getWord(%vec, 1);
+	%z = getWord(%vec, 2);
+
+	return %y SPC -%x SPC %z;
+}
+
+function Player::getLeftVector( %this )
+{
+	%vec = %this.getForwardVector();
+
+	%x = getWord(%vec, 0);
+	%y = getWord(%vec, 1);
+	%z = getWord(%vec, 2);
+
+	return -%y SPC %x SPC %z;
+}
+
+function getNumberStart( %str )
+{
+	%best = -1;
+
+	for ( %i = 0 ; %i < 10 ; %i++ )
+	{
+		%pos = strPos( %str, %i );
+
+		if ( %pos < 0 )
+		{
+			continue;
+		}
+
+		if ( %best == -1 || %pos < %best )
+		{
+			%best = %pos;
+		}
+	}
+
+	return %best;
+}
+
+function loadFootstepSounds()
+{
+	%pattern = "Add-Ons/Server_Stride/res/steps/*.wav";
+	%list = "generic 0";
+
+	deleteVariables( "$FS::Sound*" );
+	$FS::SoundNum = 0;
+
+	for ( %file = findFirstFile( %pattern ) ; %file !$= "" ; %file = findNextFile( %pattern ) )
+	{
+		%base = fileBase( %file );
+		%name = "footstepSound_" @ %base;
+
+		if ( !isObject( %name ) )
+		{
+			datablock audioProfile( genericFootstepSound )
+			{
+				description = "audioClosest3D";
+				fileName = %file;
+				preload = true;
+			};
+
+			if ( !isObject( %obj = nameToID( "genericFootstepSound" ) ) )
+			{
+				continue;
+			}
+
+			%obj.setName( %name );
+		}
+
+		if ( ( %pos = getNumberStart( %base ) ) > 0 )
+		{
+			%pre = getSubStr( %base, 0, %pos );
+			%post = getSubStr( %base, %pos, strLen( %base ) );
+
+			if ( $FS::SoundCount[ %pre ] < 1 || !strLen( $FS::SoundCount[ %pre ] ) )
+			{
+				%list = %list SPC %pre SPC $FS::SoundNum;
+			}
+
+			if ( $FS::SoundCount[ %pre ] < %post )
+			{
+				$FS::SoundCount[ %pre ] = %post;
+			}
+
+			$FS::SoundName[ $FS::SoundNum ] = %pre;
+			$FS::SoundIndex[ %pre ] = $FS::SoundNum;
+			$FS::SoundNum++;
+		}
+	}
+
+	registerOutputEvent( "fxDTSBrick", "setMaterial", "list" SPC %list TAB "bool" );
+}
+
+function fxDTSBrick::setMaterial( %this, %idx, %hasFootprints )
+{
+	%this.material = $FS::SoundName[ %idx ] TAB %hasFootprints;
+}
+
+function playFootstep( %pos, %material )
+{
+	if ( !strLen( %material ) || $FS::SoundCount[ %material ] < 1 )
+	{
+		return;
+	}
+
+	if ( !isObject( %sound = nameToID( "footstepSound_" @ %material @ getRandom( 1, $FS::SoundCount[ %material ] ) ) ) )
+	{
+		return;
+	}
+
+	serverPlay3D( %sound, %pos );
+}
+
+function Player::getFootstepSettings( %this )
+{
+	%pos = vectorAdd( %this.getPosition(), "0 0 1" );
+	%feetPos = vectorSub( %pos, "0 0 1.1" );
+	%mask = $TypeMasks::FxBrickObjectType | $TypeMasks::InteriorObjectType | $TypeMasks::TerrainObjectType | $TypeMasks::ShapeBaseObjectType;
+	return determineObjectFootstepSettings( firstWord( containerRayCast( %pos, %feetPos, %mask, %this ) ) );
+}
+
+function Player::getFootLocation( %this )
+{
+	return vectorSub( %this.getPosition(), "0 0 0.1" );
+}
+
+function Player::fs_velocityCheckTick( %this )
+{
+	cancel( %this.fs_velocityCheckTick );
+
+	if( %this.getDatablock().strideIgnore )
+		return;
+
+	if ( vectorLen( %this.getVelocity() ) >= 2.62 && !isEventPending( %this.fs_playTick ) )
+	{
+		%this.fs_playTick = %this.schedule( 100, "fs_playTick" );
+	}
+
+	%this.fs_velocityCheckTick = %this.schedule( 50, "fs_velocityCheckTick" );
+}
+
+function Player::fs_playTick( %this )
+{
+	cancel( %this.fs_playTick );
+
+	if ( vectorLen( %this.getVelocity() ) < 2.62 )
+	{
+		return;
+	}
+
+	if ( %this.fs_trigger[ 3 ] || %this.fs_trigger[ 4 ] )
+	{
+		return;
+	}
+
+	%settings = %this.getFootstepSettings();
+
+	%material = getField(%settings, 0);
+	%hasFootprints = !!getField(%settings, 1);
+	%color = getField(%settings, 2);
+
+	if ( %material $= "" )
+	{
+		return;
+	}
+
+	playFootstep( %this.getFootLocation(), %material );
+
+	if(%hasFootprints)
+	{
+		%cR = mClampF(getWord(%color, 0) - 0.075, 0, 1);
+		%cG = mClampF(getWord(%color, 1) - 0.075, 0, 1);
+		%cB = mClampF(getWord(%color, 2) - 0.075, 0, 1);
+	
+		%color = %cR SPC %cG SPC %cB SPC 1;
+	
+		%pos = vectorAdd( %this.getPosition(), "0 0 0.01" );
+	
+		%foot = !!%this.fs_currentFoot;
+	
+		if(%foot) // left foot
+		{
+			%pos = VectorAdd(%pos, VectorScale( %this.getLeftVector(), 0.25 ));
+		}
+		else // right foot
+		{
+			%pos = VectorAdd(%pos, VectorScale( %this.getRightVector(), 0.25 ));
+		}
+	
+		%obj = spawnDecal("FootprintDecal", %pos, %this.getForwardVector());
+	
+		if(isObject(%obj))
+		{
+			%obj.setNodeColor("ALL", %color);
+		}
+	
+		%this.fs_currentFoot = !%foot;
+	}
+
+	%this.fs_playTick = %this.schedule( 290, "fs_playTick" );
+}
+
+function determineObjectFootstepSettings( %obj )
+{
+	if ( !isObject( %obj ) )
+	{
+		return "";
+	}
+
+	%class = %obj.getClassName();
+
+	if ( %class $= "fxDTSBrick" )
+	{
+		%colorID = %obj.colorID;
+		%color = getColorIDTable(%colorID);
+
+		if ( strLen( %obj.material ) )
+		{
+			return getField(%obj.material, 0) TAB !!getField(%obj.material, 1) TAB mFloor(%colorID);
+		}
+
+		if($Pref::Stride::MaterialSetting[ %colorID ] !$= "")
+		{
+			return getField($Pref::Stride::MaterialSetting[ %colorID ], 0) TAB !!getField($Pref::Stride::MaterialSetting[ %colorID ], 1) TAB getColorF(%color);
+		}
+	}
+	else if ( %class $= "fxPlane" )
+	{
+		%color = %obj.color;
+	}
+
+	return getField($Pref::Stride::DefaultMaterial, 0) TAB !!getField($Pref::Stride::DefaultFootprints, 0) TAB getColorF(%color);
+}
+
+function serverCmdClearFootsteps(%this)
+{
+	if(!%this.isAdmin)
+	{
+		messageClient(%this, '', '\c7This is an admin-only command.');
+		return;
+	}
+
+	deleteVariables("$Pref::Stride::MaterialSetting*");
+
+	if(isFunction("RS_Log"))
+	{
+		// Server_Roleplay logging hook.
+		RS_Log(%this.getPlayerName() SPC "(" @ %this.getBLID() @ ") used '/clearFootsteps'");
+	}
+
+	messageAll('MsgClearBricks', '\c3%1 \c2has cleared all footstep/footprint settings.', %this.getPlayerName());
+}
+
+function serverCmdSetFootstep(%this, %material, %hasFootprints)
+{
+	if(!%this.isAdmin)
+	{
+		messageClient(%this, '', '\c7This is an admin-only command.');
+		return;
+	}
+
+	// Force to a boolean by setting hasFootprints to not-not hasFootprints.
+	%hasFootprints = !!%hasFootprints;
+
+	for(%i = 0; %i < $FS::SoundNum; %i++)
+	{
+		%name = $FS::SoundName[%i];
+		if(%name $= %material)
+		{
+			%exists = true;
+			break;
+		}
+	}
+
+	if(!%exists)
+	{
+		messageClient(%this, '', '\c7No material named \c6%1 \c7found.', StripMLControlChars(%material));
+		return;
+	}
+
+	%color = mFloor(%this.currentColor);
+
+	$Pref::Stride::MaterialSetting[%color] = %material TAB %hasFootprints;
+
+	messageClient(%this, '', '\c7You have assigned the material \c6%1 \c7to paint color <color:%2>%3\c7, with footprints \c6%4\c7.', StripMLControlChars(%material), rgbToHex(getColorIDTable(%color)), %color, %hasFootprints ? "Enabled" : "Disabled");
+
+	if(isFunction("RS_Log"))
+	{
+		// Server_Roleplay logging hook.
+		RS_Log(%this.getPlayerName() SPC "(" @ %this.getBLID() @ ") used '/setFootstep " @ %material @ "'");
+	}
+}
+
+loadFootstepSounds();
